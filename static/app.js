9a7df97e6c8aab520c663febcafa2fe42ced90bd
Трофимов Леонид Александрович <l_trofimov@med.center>
2026-03-13 15:10:21 +0300

    fix ios push subscribe

--- static/app.js
+++ static/app.js
@@ -1,12 +1,10 @@
 const publicKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvEAweJTo/7VIVeizOqiBYbiMKIwi6/CCAA9lKjQdAvMNu4kfJnkHkTn6Q32U6IybbTLJC+1w8gxmuP8QIlSezA==";
 
-
 function openModule(name){
     alert('Модуль "' + name + '" находится в разработке');
 }
 
-
-function urlBase64ToUint8Array(base64String){
+function urlBase64ToUint8Array(base64String) {
 
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
 
@@ -14,18 +12,16 @@ function urlBase64ToUint8Array(base64String){
         .replace(/\-/g, '+')
         .replace(/_/g, '/');
 
-    const rawData = window.atob(base64);
+    const rawData = atob(base64);
 
     const outputArray = new Uint8Array(rawData.length);
 
-    for (let i = 0; i < rawData.length; ++i){
+    for (let i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
 
     return outputArray;
-
 }
-
 
 async function subscribePush(){
 
@@ -34,11 +30,13 @@ async function subscribePush(){
         const permission = await Notification.requestPermission();
 
         if(permission !== "granted"){
-            alert("Разрешение на уведомления не получено");
+            alert("Уведомления не разрешены");
             return;
         }
 
         const registration = await navigator.serviceWorker.register("/static/sw.js");
+
+        await navigator.serviceWorker.ready;
 
         const subscription = await registration.pushManager.subscribe({
 
@@ -57,14 +55,14 @@ async function subscribePush(){
 
         });
 
-        alert("Уведомления включены");
+        alert("Уведомления успешно включены");
 
     }
+    catch(e){
 
-    catch(err){
+        console.error(e);
 
-        console.error(err);
-        alert("Ошибка подписки push");
+        alert("Ошибка подписки push: " + e.message);
 
     }
 
